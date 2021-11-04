c984c92a8e8718c3e8abab1eb5b4f4987ec8301f
Sergei Palmer <zigpalmer@gmail.com>
2021-11-04 14:19:29 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,28 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-// http = require('http'),
 const Notes = require('./src/notes')
-
 const notePad =  new Notes();
 
 app.use(cors());
 app.use(express.json());
-// app.configure(function(){
-//   app.use(express.static(__dirname));
-// });
-
 app.use(express.static("./"));
 
 app.get('/', function (req, res) {
   // res.send('Please visit /notes and do not panic.')
   res.render('index.html')
-  // res.sendFile('index.html');
 })
 
 app.get('/notes', function (req, res) {
   res.send(notePad.display());
-  // console.log(notePad.display());
 })
 
 app.post('/notes', function (req, res) {
